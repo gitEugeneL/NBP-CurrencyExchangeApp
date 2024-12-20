19e5ad9e9e835d28866c5b890f777470bf0e6932
Eugene L. <euglihon.dev@gmail.com>
2024-12-20 13:34:49 +0100

    added manifest

--- PWAClient/src/main.tsx
+++ PWAClient/src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- PWAClient/vite.config.ts
+++ PWAClient/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { ManifestOptions, VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+const manifest: Partial<ManifestOptions> | false = {
+  theme_color: '#6533c0',
+  background_color: '#18181e',
+  icons: [
+    {
+      purpose: 'maskable',
+      sizes: '512x512',
+      src: 'icon512_maskable.png',
+      type: 'image/png'
+    },
+    {
+      purpose: 'any',
+      sizes: '512x512',
+      src: 'icon512_rounded.png',
+      type: 'image/png'
+    }
+  ],
+  orientation: 'any',
+  display: 'standalone',
+  lang: 'en-US',
+  name: 'pwaclient',
+  short_name: 'pwa',
+  start_url: '/',
+  scope: '/'
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{html,css,js,ico,png,svg}']
+      },
+      manifest: manifest
+    })
+  ]
+});

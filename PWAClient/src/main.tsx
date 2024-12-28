53e538b7da949f1572fac621a4fb9b7d0a98ddb1
Eugene L. <euglihon.dev@gmail.com>
2024-12-28 20:52:54 +0100

    Add catch-all route to redirect to '/auth'

--- PWAClient/src/main.tsx
+++ PWAClient/src/main.tsx
@@ -75,6 +75,10 @@ const router = createBrowserRouter(
           element: <SuccessPage />
         }
       ]
+    },
+    {
+      path: '*',
+      element: <Navigate to='/auth' replace />
     }
   ],
   {

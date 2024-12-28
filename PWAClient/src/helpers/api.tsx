6f25c774e4f7e4c375aef292c1453112d2c19a7e
Eugene L. <euglihon.dev@gmail.com>
2024-12-28 21:18:49 +0100

    Test NGROK integration

--- PWAClient/src/helpers/api.tsx
+++ PWAClient/src/helpers/api.tsx
@@ -1,7 +1,7 @@
 /* local docker */
-// const dockerPort = 8000;
-// export const PREFIX = `http://localhost:${dockerPort}/api`;
+const dockerPort = 8000;
+export const PREFIX = `http://localhost:${dockerPort}/api`;
 
 /* ngrok docker */
-const ngrok = 'https://2156-79-117-68-190.ngrok-free.app';
-export const PREFIX = `${ngrok}/api`;
+// const ngrok = 'https://2156-79-117-68-190.ngrok-free.app';
+// export const PREFIX = `${ngrok}/api`;
